1043beab2c5f7dbc954a5dc74faee0ef8585c799
IvanWD <vanyal666@gmail.com>
2016-09-06 13:13:01 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,29 @@
+Vue.component('message', {
+  template: '<input v-model="message" @keyup.enter="saveMessage">',
+  data: function() {
+  return {
+      message: ''
+    }
+  },
+  methods: {
+    saveMessage: function() {
+      this.$emit('message-saved', this.message)
+
+      this.message = ""
+    }
+  }
+})
+
+new Vue({
+  el: 'body',
+  data: {
+    messages: []
+  },
+  methods: {
+    handleMessage: function(message) {
+      this.messages.push(message)
+    }
+    /*hadleMessageByButton: function(message) {
+      this.messages.push(message)*/
+  }
+})
